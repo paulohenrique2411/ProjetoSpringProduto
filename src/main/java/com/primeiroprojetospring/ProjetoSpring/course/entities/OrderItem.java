4be1fa73f5a9d30d2922228cff52e7204f492de9
Paulo Henrique Da Silva <paulo.dahsilva@redeflex.com.br>
2026-09-03 18:05:56 -0300

    Subtotal & Total methods

--- src/main/java/com/primeiroprojetospring/ProjetoSpring/course/entities/OrderItem.java
+++ src/main/java/com/primeiroprojetospring/ProjetoSpring/course/entities/OrderItem.java
@@ -63,6 +63,10 @@ public class OrderItem implements Serializable {
         this.price = price;
     }
 
+    public Double getSubTotal(){
+        return quantity * price;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
