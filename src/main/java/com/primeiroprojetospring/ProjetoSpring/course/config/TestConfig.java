82c3c7681cd9820fc9a71559aa1175eb53970854
Paulo Henrique Da Silva <paulo.dahsilva@redeflex.com.br>
2026-09-03 17:52:19 -0300

    Payment, one-to-one association

--- src/main/java/com/primeiroprojetospring/ProjetoSpring/course/config/TestConfig.java
+++ src/main/java/com/primeiroprojetospring/ProjetoSpring/course/config/TestConfig.java
@@ -26,6 +26,7 @@ public class TestConfig implements CommandLineRunner {
     @Autowired
     private OrderItemRepository orderItemRepository;
 
+
     @Override
     public void run(String... args) throws Exception {
 
@@ -66,5 +67,11 @@ public class TestConfig implements CommandLineRunner {
         OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
 
         orderItemRepository.saveAll(Arrays.asList(oi1,oi2,oi3,oi4));
+
+        Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+
+        o1.setPayment(pay1);
+
+        orderRepository.save(o1);
     }
 }
---
+++ src/main/java/com/primeiroprojetospring/ProjetoSpring/course/entities/Payment.java
@@ -0,0 +1,60 @@
+package com.primeiroprojetospring.ProjetoSpring.course.entities;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+
+import java.io.Serializable;
+import java.time.Instant;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_payment")
+public class Payment implements Serializable {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private Instant moment;
+
+    @OneToOne
+    @MapsId
+    private Order order;
+
+    public Payment(){
+
+    }
+
+    public Payment(Long id, Instant moment, Order order) {
+        this.id = id;
+        this.moment = moment;
+        this.order  = order;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Payment payment = (Payment) o;
+        return Objects.equals(id, payment.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Instant getMoment() {
+        return moment;
+    }
+
+    public void setMoment(Instant moment) {
+        this.moment = moment;
+    }
+}
