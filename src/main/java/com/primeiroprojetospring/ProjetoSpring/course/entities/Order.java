bf1e12a20fc2de5144c6bf6c8882ee265dd1f3ab
Paulo Henrique Da Silva <paulo.dahsilva@redeflex.com.br>
2026-09-03 14:35:35 -0300

    OrderStatus enum

--- src/main/java/com/primeiroprojetospring/ProjetoSpring/course/entities/Order.java
+++ src/main/java/com/primeiroprojetospring/ProjetoSpring/course/entities/Order.java
@@ -1,6 +1,7 @@
 package com.primeiroprojetospring.ProjetoSpring.course.entities;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.primeiroprojetospring.ProjetoSpring.course.entities.enums.OrderStatus;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
@@ -23,14 +24,18 @@ public class Order implements Serializable {
     @JoinColumn(name = "client_id")
     private User client;
 
+
+    private Integer orderStatus;
+
     public Order() {
 
     }
 
-    public Order(Long id, Instant moment, User client) {
+    public Order(Long id, Instant moment, OrderStatus orderStatus, User client) {
         this.id = id;
         this.moment = moment;
         this.client = client;
+        setOrderStatus(orderStatus);
     }
 
     public Long getId() {
@@ -47,6 +52,15 @@ public class Order implements Serializable {
 
     public void setMoment(Instant moment) {
         this.moment = moment;
+    }
+
+    public OrderStatus getOrderStatus() {
+        return OrderStatus.valueOf(orderStatus);
+    }
+
+    public void setOrderStatus(OrderStatus orderStatus) {
+        if(orderStatus != null)
+        this.orderStatus = orderStatus.getCode();
     }
 
     public User getClient() {
