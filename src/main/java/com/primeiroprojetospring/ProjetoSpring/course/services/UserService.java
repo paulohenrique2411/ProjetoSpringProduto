5164476365c411b3a7b65cea59fdaa3b0c66f6b4
Paulo Henrique Da Silva <paulo.dahsilva@redeflex.com.br>
2026-09-04 09:46:58 -0300

    User delete

--- src/main/java/com/primeiroprojetospring/ProjetoSpring/course/services/UserService.java
+++ src/main/java/com/primeiroprojetospring/ProjetoSpring/course/services/UserService.java
@@ -28,4 +28,6 @@ public class UserService  {
     public User insert(User obj){
         return userRepository.save(obj);
     }
+
+    public void delete(Long id) { userRepository.deleteById(id);}
 }
